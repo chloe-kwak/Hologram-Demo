b21d534b9b161d29e4fe32d46d21c16f5e88ef10
Ubuntu <ubuntu@ip-172-31-26-36.ec2.internal>
2024-10-08 01:11:17 +0000

    4th commit

--- CDK-deploy/cdk.out/asset.1c815dc5d1a1101fb6642442c5a6862d386f093f1976cb8415bd5ce97864c2fa/config_file.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    # Stack name
-    # Change this value if you want to create a new instance of the stack
-    STACK_NAME = "cdk-hologram-display"
-    
-    # Put your own custom value here to prevent ALB to accept requests from
-    # other clients that CloudFront. You can choose any random string.
-    CUSTOM_HEADER_VALUE = "My_random_value_58dsv15e4s38"    
-    
